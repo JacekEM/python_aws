3d26e7deb3d394d138f6b0ea38ce6e70ef185af8
jacek.malatynski <jacek.malatynski@jamf.com>
2018-10-27 19:02:47 +0200

    script to search for sg on AWS

---
+++ search_unused_sg/README.md
@@ -0,0 +1,12 @@
+# get_sec_groups.py 
+
+Script iterates over ec2 list and creates a set of security groups in use.
+Then runs a check against a list containing all security groups, thus 
+determinining which security groups are not used.
+
+
+**required packages**
+ - boto3
+
+
+
---
+++ search_unused_sg/get_sec_groups.py
@@ -0,0 +1,38 @@
+import boto3
+
+def main():
+    """
+    Main method, might take arguments one day.
+    Just to start it invokes get_unused_sg function.
+    """
+
+    print(get_unused_sg())
+
+def get_unused_sg():
+    """
+    Checks security groups assigned to ec2s and compares to a list
+    containing all security groups.
+    """
+
+    ec2 = boto3.client('ec2')
+    ec2_list = ec2.describe_instances()
+    all_security_groups = ec2.describe_security_groups()
+   
+    assigned_sgs = [] 
+    security_groups_ids = set()
+
+
+    for sg in all_security_groups["SecurityGroups"]:
+        security_groups_ids.add(sg["GroupId"])
+
+    for reservation in ec2_list["Reservations"]:
+        for instance in reservation["Instances"]:
+            for sg in instance["SecurityGroups"]:
+                assigned_sgs.append(sg["GroupId"])
+
+    unassigned_sg = [sg for sg in security_groups_ids if sg not in assigned_sgs]
+    
+    return unassigned_sg
+
+if __name__ ==  "__main__":
+    main()
